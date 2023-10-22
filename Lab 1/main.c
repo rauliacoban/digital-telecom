778e3b20de3de547c556f2d4ae89b3c631fdaae4
Dia <102209509+diana-tuduran@users.noreply.github.com>
2023-10-22 13:54:22 +0300

    Created the main file

---
+++ Lab 1/main.c
@@ -0,0 +1,17 @@
+/*
+main:
+{
+    open file
+    while(!EOF)
+    {
+        read char   
+        parser(char)
+        if( ready==ok)
+            print data
+        if(ready = error)
+            stop and print error message
+            "expected %c and got %c"
+    
+    }
+}
+*/
